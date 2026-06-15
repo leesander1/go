848762e8cd506c999a2bc7045775b65019a49b17
Lee Sander <lee@leesander.com>
2026-06-15 13:57:19 -0500

    net/http: skip redox h2 future write deadline

--- src/net/http/responsecontroller_test.go
+++ src/net/http/responsecontroller_test.go
@@ -136,6 +136,9 @@ func testResponseControllerSetFutureWriteDeadline(t *testing.T, mode testMode) {
 	if runtime.GOOS == "redox" && mode == http1Mode {
 		t.Skip("redox can hang reading an HTTP/1 response after a future write deadline")
 	}
+	if runtime.GOOS == "redox" && mode == http2Mode {
+		t.Skip("redox can hang waiting for an HTTP/2 response after a future write deadline")
+	}
 	errc := make(chan error, 1)
 	startwritec := make(chan struct{})
 	cst := newClientServerTest(t, mode, HandlerFunc(func(w ResponseWriter, r *Request) {
