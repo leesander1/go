234ddcc33f497ab58d73716b334c7adc2590e7ed
Lee Sander <lee@leesander.com>
2026-06-15 23:45:14 -0500

    net/http: skip redox h1 ServeContent

--- src/net/http/fs_test.go
+++ src/net/http/fs_test.go
@@ -922,6 +922,10 @@ func mustStat(t *testing.T, fileName string) fs.FileInfo {
 
 func TestServeContent(t *testing.T) { run(t, testServeContent) }
 func testServeContent(t *testing.T, mode testMode) {
+	if runtime.GOOS == "redox" && mode == http1Mode {
+		t.Skip("redox can hang completing an HTTP/1 ServeContent response")
+	}
+
 	type serveParam struct {
 		name        string
 		modtime     time.Time
