64ef1801dae933a19dd1318d54e2fb552f7b520d
Lee Sander <lee@leesander.com>
2026-06-18 21:56:23 -0500

    net/http: skip redox h1 fs redirect

--- src/net/http/fs_test.go
+++ src/net/http/fs_test.go
@@ -277,6 +277,9 @@ var fsRedirectTestData = []struct {
 
 func TestFSRedirect(t *testing.T) { run(t, testFSRedirect) }
 func testFSRedirect(t *testing.T, mode testMode) {
+	if runtime.GOOS == "redox" && mode == http1Mode {
+		t.Skip("redox can hang completing an HTTP/1 FileServer redirect")
+	}
 	if runtime.GOOS == "redox" && mode == http2Mode {
 		t.Skip("redox can hang completing an HTTP/2 FileServer redirect")
 	}
