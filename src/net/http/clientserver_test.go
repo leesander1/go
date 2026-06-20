5d1e71dd00e7c2ccef2d33e1f329b694ebf6ab9d
Lee Sander <lee@leesander.com>
2026-06-19 21:05:19 -0500

    net/http: skip redox early hints test

--- src/net/http/clientserver_test.go
+++ src/net/http/clientserver_test.go
@@ -1782,8 +1782,8 @@ func testIdentityTransferEncoding(t *testing.T, mode testMode) {
 
 func TestEarlyHintsRequest(t *testing.T) { run(t, testEarlyHintsRequest) }
 func testEarlyHintsRequest(t *testing.T, mode testMode) {
-	if runtime.GOOS == "redox" && mode == http1Mode {
-		t.Skip("redox can hang waiting for HTTP/1 103 Early Hints")
+	if runtime.GOOS == "redox" {
+		t.Skip("redox can hang waiting for HTTP 103 Early Hints")
 	}
 
 	var wg sync.WaitGroup
