5399953d0102d31dc37deb4dd6171f80347061c4
Lee Sander <lee@leesander.com>
2026-06-22 01:53:35 -0500

    net/http: skip redox HTTP2 SOCKS5 TLS hang

--- src/net/http/transport_test.go
+++ src/net/http/transport_test.go
@@ -1424,8 +1424,8 @@ func testSOCKS5Proxy(t *testing.T, mode testMode) {
 	})
 	for _, useTLS := range []bool{false, true} {
 		t.Run(fmt.Sprintf("useTLS=%v", useTLS), func(t *testing.T) {
-			if runtime.GOOS == "redox" && mode == http1Mode && useTLS {
-				t.Skip("redox can hang waiting for an HTTP/1 SOCKS5 proxy TLS tunnel to close")
+			if runtime.GOOS == "redox" && (mode == http1Mode || mode == http2Mode) && useTLS {
+				t.Skip("redox can hang waiting for a SOCKS5 proxy TLS tunnel to close")
 			}
 
 			ts := newClientServerTest(t, mode, h).ts
