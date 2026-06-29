693a767bd2e6cdfa3e7d7224f7c49d2f559a855e
Lee Sander <lee@leesander.com>
2026-06-29 03:55:44 -0500

    net/http: skip redox ParseForm cleanup hang

--- src/net/http/serve_test.go
+++ src/net/http/serve_test.go
@@ -7397,6 +7397,9 @@ func testParseFormCleanup(t *testing.T, mode testMode) {
 	if mode == http2Mode {
 		t.Skip("https://go.dev/issue/20253")
 	}
+	if runtime.GOOS == "redox" && mode == http1Mode {
+		t.Skip("redox can hang cleaning up multipart form temporary files after an HTTP/1 request")
+	}
 
 	const maxMemory = 1024
 	const key = "file"
