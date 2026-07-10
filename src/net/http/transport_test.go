5caa180cfbf1a2b5f843e53abff5243cdb878653
Lee Sander <lee@leesander.com>
2026-07-10 14:04:06 -0500

    net/http: skip redox max conns h2 dial hang

--- src/net/http/transport_test.go
+++ src/net/http/transport_test.go
@@ -601,6 +601,9 @@ func testTransportMaxConnsPerHostIncludeDialInProgress(t *testing.T, mode testMo
 	if runtime.GOOS == "redox" && mode == http1Mode {
 		t.Skip("redox can hang while including in-progress HTTP/1 dials in MaxConnsPerHost")
 	}
+	if runtime.GOOS == "redox" && mode == http2Mode {
+		t.Skip("redox can hang while including in-progress HTTP/2 dials in MaxConnsPerHost")
+	}
 
 	ts := newClientServerTest(t, mode, HandlerFunc(func(w ResponseWriter, r *Request) {
 		_, err := w.Write([]byte("foo"))
