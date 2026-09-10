c89eb5137ba8882f67558ac528df3203c648d96a
Lee Sander <lee@leesander.com>
2026-09-10 15:13:22 -0500

    net/http: enable response body close test on redox

--- src/net/http/transport_test.go
+++ src/net/http/transport_test.go
@@ -3059,9 +3059,6 @@ func testTransportCancelRequestBeforeResponseHeaders(t *testing.T, test cancelTe
 // Now it actually closes the TCP connection.
 func TestTransportCloseResponseBody(t *testing.T) { run(t, testTransportCloseResponseBody) }
 func testTransportCloseResponseBody(t *testing.T, mode testMode) {
-	if runtime.GOOS == "redox" {
-		t.Skip("redox can hang waiting for server write failure after closing the response body")
-	}
 	writeErr := make(chan error, 1)
 	msg := []byte("young\n")
 	ts := newClientServerTest(t, mode, HandlerFunc(func(w ResponseWriter, r *Request) {
