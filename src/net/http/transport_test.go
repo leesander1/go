b9bb2cb8d3866bd2b2369fb98404ff8e4ad1d883
Lee Sander <lee@leesander.com>
2026-09-10 15:02:51 -0500

    net/http: enable custom dial tests on redox

--- src/net/http/transport_test.go
+++ src/net/http/transport_test.go
@@ -4161,9 +4161,6 @@ func testTransportClosesBodyOnError(t *testing.T, mode testMode) {
 }
 
 func TestTransportDialTLS(t *testing.T) {
-	if runtime.GOOS == "redox" {
-		t.Skip("redox can cause a package-level transport failure after custom TLS dialing")
-	}
 	run(t, testTransportDialTLS, []testMode{https1Mode, http2Mode})
 }
 func testTransportDialTLS(t *testing.T, mode testMode) {
@@ -4202,9 +4199,6 @@ func testTransportDialTLS(t *testing.T, mode testMode) {
 }
 
 func TestTransportDialContext(t *testing.T) {
-	if runtime.GOOS == "redox" {
-		t.Skip("redox can cause a package-level transport failure after custom context dialing")
-	}
 	run(t, testTransportDialContext)
 }
 func testTransportDialContext(t *testing.T, mode testMode) {
@@ -4249,9 +4243,6 @@ func testTransportDialContext(t *testing.T, mode testMode) {
 }
 
 func TestTransportDialTLSContext(t *testing.T) {
-	if runtime.GOOS == "redox" {
-		t.Skip("redox can cause a package-level transport failure after custom context TLS dialing")
-	}
 	run(t, testTransportDialTLSContext, []testMode{https1Mode, http2Mode})
 }
 func testTransportDialTLSContext(t *testing.T, mode testMode) {
