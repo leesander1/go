8984bb090ce69734d54c9695b8ead567977c2743
Lee Sander <lee@leesander.com>
2026-09-10 17:29:52 -0500

    net: enable TCPConn and RawConn tests on redox

--- src/net/protoconn_test.go
+++ src/net/protoconn_test.go
@@ -62,10 +62,6 @@ func TestTCPListenerSpecificMethods(t *testing.T) {
 }
 
 func TestTCPConnSpecificMethods(t *testing.T) {
-	if runtime.GOOS == "redox" {
-		t.Skip("redox aborts runtime netpoll while exercising TCPConn specific methods")
-	}
-
 	la, err := ResolveTCPAddr("tcp4", "127.0.0.1:0")
 	if err != nil {
 		t.Fatal(err)
