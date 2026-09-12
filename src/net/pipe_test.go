25928386e59672431ef2d4c14b83b79f627d3ff7
Lee Sander <lee@leesander.com>
2026-09-12 13:02:48 -0500

    runtime: fix Redox realtime clock ID

--- src/net/pipe_test.go
+++ src/net/pipe_test.go
@@ -7,7 +7,6 @@ package net_test
 import (
 	"io"
 	"net"
-	"runtime"
 	"testing"
 	"time"
 
@@ -15,11 +14,6 @@ import (
 )
 
 func TestPipe(t *testing.T) {
-	switch runtime.GOOS {
-	case "redox":
-		t.Skipf("not supported on %s", runtime.GOOS)
-	}
-
 	nettest.TestConn(t, func() (c1, c2 net.Conn, stop func(), err error) {
 		c1, c2 = net.Pipe()
 		stop = func() {
