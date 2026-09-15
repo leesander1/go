07a3f7d2a05b9fa77cb970bc9a4bc0c8c4f24323
Lee Sander <lee@leesander.com>
2026-09-14 21:48:23 -0500

    os: enable Redox pipe EOF test

--- src/os/pipe_test.go
+++ src/os/pipe_test.go
@@ -348,10 +348,6 @@ func testCloseWithBlockingRead(t *testing.T, r, w *os.File) {
 }
 
 func TestPipeEOF(t *testing.T) {
-	if runtime.GOOS == "redox" {
-		t.Skip("Redox pipe writer close does not unblock readers with EOF yet")
-	}
-
 	t.Parallel()
 
 	r, w, err := os.Pipe()
